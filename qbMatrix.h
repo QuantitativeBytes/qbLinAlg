82bb20727bc98d3cc33360106725ab8213c33727
QuantitativeBytes <QuantitativeBytes@gmail.com>
2023-01-28 15:40:57 +0000

    Changes to qbVector.h and qbMatrix.h to improve performance. Modified comments to include license terms in all files.

--- qbMatrix.h
+++ qbMatrix.h
@@ -1,5 +1,25 @@
 // This file is part of the qbLinAlg linear algebra library.
 
+/*
+MIT License
+Copyright (c) 2023 Michael Bennett	
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #ifndef QBMATRIX2_H
 #define QBMATRIX2_H
 
@@ -554,14 +574,15 @@ qbMatrix2<T> qbMatrix2<T>::operator= (const qbMatrix2<T> &rhs)
 	return *this;
 }
 */
+
 template <class T>
 qbMatrix2<T> qbMatrix2<T>::operator= (const qbMatrix2<T> &rhs)
 {
 	// Make sure we're not assigning to ourself.
 	if (this != &rhs)
 	{
-		/* If the dimensions are the same, we only need to copy the elements,
-			there is no need to delete and re-allocate memory. */
+		// If the dimensions are the same, we only need to copy the elements,
+		//	there is no need to delete and re-allocate memory.
 		if ((m_nRows == rhs.m_nRows) && (m_nCols == rhs.m_nCols))
 		{
 			for (int i=0; i<m_nElements; ++i)
@@ -584,6 +605,7 @@ qbMatrix2<T> qbMatrix2<T>::operator= (const qbMatrix2<T> &rhs)
 	
 	return *this;
 }
+
 
 /* **************************************************************************************************
 SEPARATE THE MATRIX INTO TWO PARTS, AROUND THE COLUMN NUMBER PROVIDED
